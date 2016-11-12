d7838f3f06d5dfda0e50c1d5aefb8a3975a4da3b
imperatrixmundi <physicist.moni@gmail.com>
2016-11-12 16:47:05 -0500

    Some text

---
+++ app/src/main/java/com/example/android/friendcatalog/Intro.java
@@ -0,0 +1,20 @@
+package com.example.android.friendcatalog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class Intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+
+    public void HobbiesCollect (View view){
+
+
+    }
+}
